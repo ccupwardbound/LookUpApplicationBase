9bf6893796aaf17ad9505a81c705167520adf71c
NOTO-Upwards <53187736+NOTO-Upwards@users.noreply.github.com>
2019-07-22 13:58:27 -0700

    Initial Commit
    Base Application. Proof of concept.

---
+++ Upward Bound Base/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManagement : MonoBehaviour
+{
+    public Text NameOfItem;
+    public Text BackgroundInfo;
+    public Text Address;
+    public Text City;
+    public Text State;
+    public Text ZIP;
+    public Text Title;
+    public GameObject InfoCanvas;
+
+    public List<ItemInfo> ItemInfos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var dataAsString = GatherData.GetDataAsStringFromJsonFile(GatherData.GetFilePath());
+        //ItemInfos = GatherData.GetDeserializedData(dataAsString);
+
+        ItemInfos = GatherData.GetDeserializedData(GatherData.GetTextAsset().text);
+
+        NameOfItem.text = "this is the name of an item";
+        BackgroundInfo.text = "this is background info";
+        Address.text = "this is the address";
+        City.text = "this is the city";
+        State.text = "this is the state";
+        ZIP.text = "this is the zip";
+        Title.text = "This is what you are looking at!";
+
+        InfoCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ThingWeFound = DefaultTrackableEventHandler.ThingWeFound;
+        if (!string.IsNullOrEmpty(ThingWeFound) || !string.IsNullOrWhiteSpace(ThingWeFound))
+        {
+            foreach (var item in ItemInfos)
+            {
+                if (item.ID.Equals(ThingWeFound))
+                {
+                    NameOfItem.text = item.Name;
+                    BackgroundInfo.text = item.Description.BackgroundInfo;
+                    Address.text = item.Description.Location.Address;
+                    City.text = item.Description.Location.City;
+                    State.text = item.Description.Location.State;
+                    ZIP.text = item.Description.Location.ZIP;
+                    break;
+                }
+            }
+            Debug.Log("ThingWeFound: " + ThingWeFound);
+            ThingWeFound = "";
+            InfoCanvas.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Nothing here ;)");
+            InfoCanvas.SetActive(false);
+        }
+
+    }
+}
---
+++ Upward Bound Base/Assets/StartCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCamera : MonoBehaviour
+{
+    public void StartTheCamera()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Lookup_Finder");
+    }
+}
